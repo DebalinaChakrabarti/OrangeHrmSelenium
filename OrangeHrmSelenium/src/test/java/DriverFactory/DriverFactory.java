59cbda1618ec9a6ee2d22312afe4f4c6422c1524
DebalinaChakrabarti <debalinachakrabarti@gmail.com>
2024-05-28 23:53:18 -0500

    Project Structure first commit

---
+++ OrangeHrmSelenium/src/test/java/DriverFactory/DriverFactory.java
@@ -0,0 +1,85 @@
+package DriverFactory;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DriverFactory {
+
+	public WebDriver driver;
+	public final static int TIMEOUT = 50;
+	
+	public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+	
+	/**
+	 * This method is used to initialize the threadlocal driver on the basis of given browser
+	 * 
+	 * @param browser
+	 * @return this will return tldriver
+	 */
+	
+	public WebDriver init_driver(String browser)
+	{
+		System.out.println("browser value is:"+browser);
+//		browser = System.getenv("BROWSER");
+		if(browser.equals("chrome"))
+		{
+
+		//	ChromeOptions opt = new ChromeOptions();
+//			WebDriverManager.chromedriver().setup();
+
+			//System.setProperty("webdriver.chrome.driver","../../Downloads/chromedriver-mac-x64/chromedriver");
+			//System.out.println("Updating Chrome Version");
+
+			//ChromeOptions options = new ChromeOptions();
+
+			//options.addArguments("--no-sandbox");
+			//options.addArguments("--disable-dev-shm-usage");
+			//options.addArguments("--headless");
+			driver= new ChromeDriver();
+			tlDriver.set(driver);
+
+			
+		}
+		else if(browser.equals("firefox"))
+		{
+			WebDriverManager.firefoxdriver().setup();
+			tlDriver.set(new FirefoxDriver());
+		}
+		else if(browser.equals("safari"))
+		{
+			tlDriver.set(new SafariDriver());  
+		}
+		else {
+			System.out.println("Please pass the correct browser value " + browser);
+		}
+//		new WebDriverWait(tlDriver, Duration.ofSeconds(TIMEOUT));
+		getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(TIMEOUT));
+
+		getDriver().manage().deleteAllCookies();
+		getDriver().manage().window().maximize();
+		return getDriver();
+	}
+	/**
+	 * this method is used to get the driver with ThreadLocal
+	 * synchronized keyword is used to synchronize the multiple WebDriver if they run parallel
+	 * @return
+	 */
+	public static synchronized WebDriver getDriver() {
+		
+		return tlDriver.get();
+		
+	}
+	
+	
+	
+	
+}
+ 
---
+++ OrangeHrmSelenium/src/test/java/Utilities/Constants.java
@@ -0,0 +1,11 @@
+package Utilities;
+
+public class Constants {
+	
+	public static final String baseUrl = "https://lms-frontend-api-hackathon-apr-326235f3973d.herokuapp.com/";
+	public static final String invalidbaseUrl="https://lms-frontend-api-hackathon-apr-326235f393d.herokuapp.com/";
+	
+
+
+}
+	
